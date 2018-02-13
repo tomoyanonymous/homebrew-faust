5632a5b848f67660fbe1816e84b258f36673ce5d
tomoyanonymous <anon.spyc@gmail.com>
2018-02-13 17:44:08 +0900

    ver.2.5.21

--- faust.rb
+++ faust.rb
@@ -1,8 +1,8 @@
 class Faust < Formula
   desc "Functional AUdio STream is language for signal processing and synthesis"
   homepage "http://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.5.17/faust-2.5.17.tar.gz"
-  sha256 "5e9e76dc4754efbf31eb8b63ee4f3e2e5ec5f6fe20dac611af859357bf4a1893"
+  url "https://github.com/grame-cncm/faust/releases/download/2.5.21/faust-2.5.21.tar.gz"
+  sha256 "6de2214316f9137cabf95bf075a599963cfe5305ab83d944e869bec417956f2f"
   head "https://github.com/grame-cncm/faust.git", :branch => "master-dev"
 
   option "with-web", "will install asmjs and wasm compiler."
