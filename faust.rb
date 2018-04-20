9fee833f7f830e803b51f03d0d763127b3a68be2
tomoyanonymous <anon.spyc@gmail.com>
2018-04-21 03:47:11 +0900

    updated following to 2.5.23(now we need cmake to build)

--- faust.rb
+++ faust.rb
@@ -1,14 +1,15 @@
 class Faust < Formula
   desc "Functional AUdio STream is language for signal processing and synthesis"
   homepage "http://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.5.21/faust-2.5.21.tar.gz"
-  sha256 "6de2214316f9137cabf95bf075a599963cfe5305ab83d944e869bec417956f2f"
+  url "https://github.com/grame-cncm/faust/releases/download/2.5.23/faust-2.5.23.tar.gz"
+  sha256 "d87ef98a7a25367c12f51fe1ebb07fe4f2c0b37f27b63b7bcd074273b295e5fb"
   head "https://github.com/grame-cncm/faust.git", :branch => "master-dev"
 
-  option "with-web", "will install asmjs and wasm compiler."
-  option "with-ios", "will not install ios and ios-llvm compiler."
-  option "with-universal", "create universal binary"
+  option "with-web", "install with asmjs and wasm compiler(need emscripten)."
+  option "with-ios", "install with ios libraly(only in head)"
+  option "with-universal", "create universal binary(only in head)"
 
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "llvm"
   depends_on "openssl"
@@ -21,23 +22,14 @@ class Faust < Formula
   def install
     if build.with? "universal"
       system "make", "universal"
-      system "make", "sound2faust"
-      system "make", "httpd"
-      system "make", "dynamic"
-    else
-      system "make", "world"
     end
+    system "make", "all"
     if build.with? "web"
-      system "make", "asmjs"
       system "make", "wasm"
+      system "make", "asmjs"
     end
     if build.with? "ios"
       system "make", "ios"
-      system "make", "ios-llvm"
-    end
-    system "make", "install", "PREFIX=#{prefix}"
-    if build.with? "test"
-      cp_r "tests", prefix
     end
   end
   test do
