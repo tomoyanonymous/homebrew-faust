c1803a31c9a986f323c8d96ce1ef519098f77562
tomoyanonymous <anon.spyc@gmail.com>
2019-04-10 10:59:43 +0900

    hash fix and audit

--- faust.rb
+++ faust.rb
@@ -2,7 +2,7 @@ class Faust < Formula
   desc "Functional AUdio STream is language for signal processing and synthesis"
   homepage "http://faust.grame.fr"
   url "https://github.com/grame-cncm/faust/archive/2.15.11.tar.gz"
-  sha256 "910c65a851fdbf1a289dea8df204d48e2065a137"
+  sha256 "4e6b8968679bd3f7b1a5fa47752ebc50b2a27ecb88004af181deee578131e42c"
   head "https://github.com/grame-cncm/faust.git", :branch => "master-dev"
 
   option "with-web", "install with  wasm compiler(need emscripten)."
@@ -12,13 +12,12 @@ class Faust < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "llvm"
-  depends_on "openssl"
   depends_on "libmicrohttpd"
   depends_on "libsndfile"
   if build.with? "web"
     depends_on "emscripten" => :build
   end
-
+  depends_on "openssl"
   def install
     if build.with? "universal"
       system "make", "universal"
