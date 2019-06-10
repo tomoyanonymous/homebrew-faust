efde02e4979411a0b588c0076046c04337cb9bdb
tomoyanonymous <anon.spyc@gmail.com>
2019-06-10 13:45:11 +0900

    changed source file url

--- faust.rb
+++ faust.rb
@@ -1,8 +1,8 @@
 class Faust < Formula
   desc "Functional AUdio STream is language for signal processing and synthesis"
   homepage "http://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/archive/2.15.11.tar.gz"
-  sha256 "4e6b8968679bd3f7b1a5fa47752ebc50b2a27ecb88004af181deee578131e42c"
+  url "https://github.com/grame-cncm/faust/releases/download/2.15.11/faust-2.15.11.tar.gz"
+  sha256 "660816b7fa44da718868d895a82f18f4a441c347bc39afc20a651124f5711d5c"
   head "https://github.com/grame-cncm/faust.git", :branch => "master-dev"
 
   option "with-web", "install with  wasm compiler(need emscripten)."
