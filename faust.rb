9b9e5f032e893c903cdd2461696a8f717c5c9355
tomoyanonymous <anon.spyc@gmail.com>
2020-01-18 20:14:36 +0900

    deleted deprecated options and overwrite max make_jobs

--- faust.rb
+++ faust.rb
@@ -4,30 +4,17 @@ class Faust < Formula
   url "https://github.com/grame-cncm/faust/releases/download/2.15.11/faust-2.15.11.tar.gz"
   sha256 "660816b7fa44da718868d895a82f18f4a441c347bc39afc20a651124f5711d5c"
   head "https://github.com/grame-cncm/faust.git", :branch => "master-dev"
-
-  option "with-web", "install with  wasm compiler(need emscripten)."
-  option "with-ios", "install with ios library"
-  option "with-universal", "create universal binary"
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "libmicrohttpd"
-  depends_on "libsndfile"
-  if build.with? "web"
-    depends_on "emscripten" => :build
-  end
-  depends_on "llvm"
-  depends_on "openssl"
+  depends_on "libmicrohttpd" => :build
+  depends_on "libsndfile" => :build
+  depends_on "llvm" =>:build
+  depends_on "openssl" => :build
   def install
     if build.with? "universal"
       system "make", "universal"
     end
-    system "make", "all"
-    if build.with? "web"
-      system "make", "wasm"
-    end
-    if build.with? "ios"
-      system "make", "ios"
-    end
+    system "make", "all" , "JOBS=${HOMEBREW_MAKE_JOBS}"
     system "make", "install", "PREFIX=#{prefix}"
   end
   test do
